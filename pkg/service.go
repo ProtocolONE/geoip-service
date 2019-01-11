e0d7d798f137543a9f29022e8b835caa265fc7df
Nikolay Bondarenko <misterionkell@gmail.com>
2019-01-11 21:34:57 +0300

    Add service and tests

---
+++ pkg/service.go
@@ -0,0 +1,84 @@
+package geoip
+
+import (
+	"context"
+	"fmt"
+	"github.com/ProtocolONE/geoip-service/pkg/proto"
+	"github.com/oschwald/geoip2-golang"
+	"net"
+)
+
+const (
+	Version     = "latest"
+	ServiceName = "go.p1.geoip-service"
+)
+
+type Service struct {
+	GeoReader *geoip2.Reader
+}
+
+func (c *Service) GetIpData(ctx context.Context, req *proto.GeoIpDataRequest, res *proto.GeoIpDataResponse) error {
+	ip := net.ParseIP(req.IP)
+	if ip == nil {
+		return fmt.Errorf("%s is invalid IP adress", req.IP)
+	}
+
+	data, err := c.GeoReader.City(ip)
+	if err != nil {
+		return err
+	}
+
+	res.Continent = &proto.GeoIpContinent{
+		GeoNameID: uint32(data.Continent.GeoNameID),
+		Code:      data.Continent.Code,
+		Names:     data.Continent.Names,
+	}
+
+	res.City = &proto.GeoIpCity{
+		GeoNameID: uint32(data.City.GeoNameID),
+		Names:     data.City.Names,
+	}
+
+	res.Country = &proto.GeoIpCountry{
+		GeoNameID:         uint32(data.Country.GeoNameID),
+		IsoCode:           data.Country.IsoCode,
+		IsInEuropeanUnion: data.Country.IsInEuropeanUnion,
+		Names:             data.Country.Names,
+	}
+
+	/*
+	   This is used when the IP address belongs to something like a military base. The represented_country is
+	   the country that the base represents. This can be useful for managing content licensing, among other uses.
+	*/
+	res.RepresentedCountry = &proto.GeoIpRepresentedCountry{
+		GeoNameID:         uint32(data.RepresentedCountry.GeoNameID),
+		IsoCode:           data.RepresentedCountry.IsoCode,
+		IsInEuropeanUnion: data.RepresentedCountry.IsInEuropeanUnion,
+		Names:             data.RepresentedCountry.Names,
+		Type:              data.RepresentedCountry.Type,
+	}
+
+	res.Location = &proto.GeoIpLocation{
+		AccuracyRadius: uint32(data.Location.AccuracyRadius),
+		Latitude:       data.Location.Latitude,
+		Longitude:      data.Location.Longitude,
+		MetroCode:      uint32(data.Location.MetroCode),
+		TimeZone:       data.Location.TimeZone,
+	}
+
+	res.Postal = &proto.GeoIpPostal{Code: data.Postal.Code}
+
+	if len(data.Subdivisions) > 0 {
+		res.Subdivisions = make([]*proto.GeoIpSubdivision, len(data.Subdivisions))
+
+		for index, subdiv := range data.Subdivisions {
+			res.Subdivisions[index] = &proto.GeoIpSubdivision{
+				GeoNameID: uint32(subdiv.GeoNameID),
+				IsoCode:   subdiv.IsoCode,
+				Names:     subdiv.Names,
+			}
+		}
+	}
+
+	return nil
+}
---
+++ pkg/service_test.go
@@ -0,0 +1,86 @@
+package geoip_test
+
+import (
+	"context"
+	"github.com/ProtocolONE/geoip-service/pkg"
+	"github.com/ProtocolONE/geoip-service/pkg/proto"
+	"github.com/oschwald/geoip2-golang"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"net"
+	"testing"
+)
+
+type ServiceTestSuite struct {
+	suite.Suite
+	service *geoip.Service
+	db      *geoip2.Reader
+}
+
+func Test_GameService(t *testing.T) {
+	suite.Run(t, new(ServiceTestSuite))
+}
+
+func (suite *ServiceTestSuite) SetupTest() {
+	db, err := geoip2.Open("../assets/GeoLite2-City.mmdb")
+	if err != nil {
+		suite.Fail("Unable to open database")
+	}
+
+	suite.db = db
+	suite.service = &geoip.Service{GeoReader: db}
+}
+
+func (suite *ServiceTestSuite) TearDownTest() {
+	if err := suite.db.Close(); err != nil {
+		panic(err)
+	}
+}
+
+func (suite *ServiceTestSuite) TestWrongIpAddress() {
+	err := suite.service.GetIpData(context.TODO(), &proto.GeoIpDataRequest{IP: "---"}, &proto.GeoIpDataResponse{})
+	assert.Error(suite.T(), err, "Return error with wrong IP")
+}
+
+func (suite *ServiceTestSuite) TestRightIpAddress() {
+	testIp := "136.0.16.217"
+	response := &proto.GeoIpDataResponse{}
+
+	origin, err := suite.db.City(net.ParseIP(testIp))
+	assert.NoError(suite.T(), err)
+
+	err = suite.service.GetIpData(context.TODO(), &proto.GeoIpDataRequest{IP: testIp}, response)
+	assert.NoError(suite.T(), err)
+
+	assert.EqualValues(suite.T(), response.City.GeoNameID, origin.City.GeoNameID)
+	assert.EqualValues(suite.T(), response.City.Names, origin.City.Names)
+
+	assert.EqualValues(suite.T(), response.Continent.GeoNameID, origin.Continent.GeoNameID)
+	assert.EqualValues(suite.T(), response.Continent.Code, origin.Continent.Code)
+	assert.EqualValues(suite.T(), response.Continent.Names, origin.Continent.Names)
+
+	assert.EqualValues(suite.T(), response.Country.GeoNameID, origin.Country.GeoNameID)
+	assert.EqualValues(suite.T(), response.Country.Names, origin.Country.Names)
+	assert.EqualValues(suite.T(), response.Country.IsoCode, origin.Country.IsoCode)
+	assert.EqualValues(suite.T(), response.Country.IsInEuropeanUnion, origin.Country.IsInEuropeanUnion)
+
+	assert.EqualValues(suite.T(), response.RepresentedCountry.GeoNameID, origin.RepresentedCountry.GeoNameID)
+	assert.EqualValues(suite.T(), response.RepresentedCountry.Names, origin.RepresentedCountry.Names)
+	assert.EqualValues(suite.T(), response.RepresentedCountry.IsoCode, origin.RepresentedCountry.IsoCode)
+	assert.EqualValues(suite.T(), response.RepresentedCountry.IsInEuropeanUnion, origin.RepresentedCountry.IsInEuropeanUnion)
+	assert.EqualValues(suite.T(), response.RepresentedCountry.Type, origin.RepresentedCountry.Type)
+
+	assert.EqualValues(suite.T(), response.Postal.Code, origin.Postal.Code)
+
+	assert.EqualValues(suite.T(), response.Location.AccuracyRadius, origin.Location.AccuracyRadius)
+	assert.EqualValues(suite.T(), response.Location.Longitude, origin.Location.Longitude)
+	assert.EqualValues(suite.T(), response.Location.Latitude, origin.Location.Latitude)
+	assert.EqualValues(suite.T(), response.Location.TimeZone, origin.Location.TimeZone)
+	assert.EqualValues(suite.T(), response.Location.MetroCode, origin.Location.MetroCode)
+
+	for i, subdiv := range origin.Subdivisions {
+		assert.EqualValues(suite.T(), response.Subdivisions[i].GeoNameID, subdiv.GeoNameID)
+		assert.EqualValues(suite.T(), response.Subdivisions[i].IsoCode, subdiv.IsoCode)
+		assert.EqualValues(suite.T(), response.Subdivisions[i].Names, subdiv.Names)
+	}
+}
